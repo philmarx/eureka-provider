96e2420a50e0a917162d9f4748d2359f3ad1d3cf
马健原 <mjy@yueyuan.pro>
2017-06-30 16:56:36 +0800

    Signed-off-by: 马健原 <mjy@yueyuan.pro>

---
+++ src/main/java/com/study/web/HelloController.java
@@ -0,0 +1,11 @@
+package com.study.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	private final static Log log=LogFactory.getLog(HelloController.class);
+	
+}
