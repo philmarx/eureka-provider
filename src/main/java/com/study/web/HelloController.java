9299722cd07a0230d91e5b2a474ffbe13d3cb5bd
马健原 <mjy@yueyuan.pro>
2017-06-30 18:32:19 +0800

    Signed-off-by: 马健原 <mjy@yueyuan.pro>

--- src/main/java/com/study/web/HelloController.java
+++ src/main/java/com/study/web/HelloController.java
@@ -2,10 +2,24 @@ package com.study.web;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	private final static Log log=LogFactory.getLog(HelloController.class);
 	
+	@Autowired
+	DiscoveryClient client;
+	
+	@GetMapping("hello")
+	public String index(){
+		ServiceInstance instance=client.getLocalServiceInstance();
+		String msg="/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId();
+		log.info(msg);
+		return msg;
+	}
 }
